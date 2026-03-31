0ac0cf67096de9328df30fb012cbe58fee8d1b65
Chris Alder <chris.alder@nanoporetech.com>
2026-03-31 11:27:48 +0000

    Validate barcode-less sample sheets (Don't mess with ingress) [CW-7025]

--- bin/workflow_glue/tests/wfg_helpers/test_check_sample_sheet.py
+++ bin/workflow_glue/tests/wfg_helpers/test_check_sample_sheet.py
@@ -10,35 +10,91 @@ from workflow_glue.wfg_helpers import check_sample_sheet
 # in case the error message is customized by `check_sample_sheet.py`) and required
 # sample types to be tested.
 ERROR_MESSAGES = [
-    ("sample_sheet_1.csv", "", ""),
+    ("sample_sheet_1.csv", "", "", False),
     # check required sample types
-    ("sample_sheet_1.csv", "Sample sheet requires at least 1 of ", "positive_control"),
-    ("sample_sheet_1.csv", "Not an allowed sample type: ", "invalid_sample_type"),
+    (
+        "sample_sheet_1.csv",
+        "Sample sheet requires at least 1 of ",
+        "positive_control",
+        False,
+    ),
+    (
+        "sample_sheet_1.csv",
+        "Not an allowed sample type: ",
+        "invalid_sample_type",
+        False),
     # check sample sheet structure etc.
-    ("sample_sheet_2.csv", "'barcode' column missing", ""),
-    ("sample_sheet_3.csv", "'alias' column missing", ""),
-    ("sample_sheet_4.csv", "values in 'barcode' column not unique", ""),
-    ("sample_sheet_5.csv", "values in 'alias' column not unique", ""),
-    ("sample_sheet_6.csv", "Unexpected number of cells in row number 2", ""),
-    ("sample_sheet_7.csv", "found unexpected values in 'type' column: ", ""),
-    ("sample_sheet_8.csv", "values in 'barcode' column are different lengths", ""),
-    ("sample_sheet_9.csv", "values in 'barcode' column are incorrect format", ""),
+    ("sample_sheet_2.csv", "'barcode' column missing", "", False),
+    ("sample_sheet_3.csv", "'alias' column missing", "", False),
+    ("sample_sheet_4.csv", "values in 'barcode' column not unique", "", False),
+    ("sample_sheet_5.csv", "values in 'alias' column not unique", "", False),
+    (
+        "sample_sheet_6.csv",
+        "Unexpected number of cells in row number 1",
+        "",
+        False,
+    ),
+    ("sample_sheet_7.csv", "found unexpected values in 'type' column: ", "", False),
+    (
+        "sample_sheet_8.csv",
+        "values in 'barcode' column are different lengths",
+        "",
+        False,
+    ),
+    (
+        "sample_sheet_9.csv",
+        "values in 'barcode' column are incorrect format",
+        "",
+        False,
+    ),
     # misc
-    ("missing.csv", "Could not open sample sheet", ""),
-    ("utf8_bom.csv", "", ""),  # check this does not fail
+    ("missing.csv", "Could not open sample sheet", "", False),
+    ("utf8_bom.csv", "", "", False),  # check this does not fail
     # check other input formats
-    ("sample_sheet_1.csv.zip", "The sample sheet doesn't seem to be a CSV file.", ""),
-    ("sample_sheet_1.xlsx", "The sample sheet doesn't seem to be a CSV file.", ""),
-    ("sample_sheet_10.csv", "", ""),  # check this does not fail
+    (
+        "sample_sheet_1.csv.zip",
+        "The sample sheet doesn't seem to be a CSV file.",
+        "",
+        False,
+    ),
+    (
+        "sample_sheet_1.xlsx",
+        "The sample sheet doesn't seem to be a CSV file.",
+        "",
+        False,
+    ),
+    ("sample_sheet_10.csv", "", "", False),  # check this does not fail
     (
         "sample_sheet_11.csv",
         "if an 'analysis_group' column exists, it needs values in each row",
         "",
+        False,
     ),
     (
         "sample_sheet_12.csv",
         "values in 'alias' column must not begin with 'barcode'",
-        ""
+        "",
+        False,
+    ),
+    # no_barcode tests
+    (
+        "sample_sheet_13.csv",
+        "'alias' column must not be present with --no_barcode",
+        "",
+        True,
+    ),
+    ("sample_sheet_14.csv", "", "", True),
+    (
+        "sample_sheet_15.csv",
+        "'alias' column must not be present with --no_barcode",
+        "",
+        True,
+    ),
+    (
+        "sample_sheet_16.csv",
+        "'barcode' column must not be present with --no_barcode",
+        "",
+        True,
     ),
 ]
 
@@ -52,15 +108,18 @@ def test_data(request):
         "check_sample_sheet")
 
 
-@pytest.mark.parametrize("sample_sheet_name,error_msg,required_types", ERROR_MESSAGES)
+@pytest.mark.parametrize(
+        "sample_sheet_name,error_msg,required_types,no_barcode", ERROR_MESSAGES)
 def test_check_sample_sheet(
-        capsys, test_data, sample_sheet_name, error_msg, required_types):
+        capsys, test_data, sample_sheet_name, error_msg, required_types, no_barcode):
     """Test the sample sheets."""
     expected_error_message = error_msg
     sample_sheet_path = f"{test_data}/{sample_sheet_name}"
     args = [sample_sheet_path]
     if required_types:
         args += ['--required_sample_types', required_types]
+    if no_barcode:
+        args += ['--no_barcode']
     parsed_args = check_sample_sheet.argparser().parse_args(args)
     try:
         check_sample_sheet.main(parsed_args)
